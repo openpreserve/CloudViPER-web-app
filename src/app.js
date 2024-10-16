e0ed9cb66d626d3039fc586c445714ac28edc4ca
Darren Dignam <darren.dignam@gmail.com>
2024-10-16 15:11:42 +0200

    Added dependencies to app init

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const dotenv = require('dotenv').config();
-
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const MemoryStore = require('memorystore')(expressSession);
+const passport = require('passport');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+// Begin server setup
+app.use( bodyParser.urlencoded({ extended: true}) );
 var path = require('path');
-
 app.disable('x-powered-by');
 app.set('trust proxy', 1);
 
+//  sessions
+var session_config = {
+    name:"vipercloud.sid",
+    cookie: { maxAge: ((4 * 24) * 60 * 60 * 1000) }, // 4 days
+    store: new MemoryStore({ checkPeriod: 86400000 }), // prune expired entries every 24h
+    secret: process.env.APP_COOKIE_SECRET, // Replace with your own secret key
+    resave: false,
+    saveUninitialized: false,
+}
+// Prod specific 
 if (process.env.NODE_ENV === 'production') {
+    session_config.cookie.secure = true;
     app.use((req, res, next)=>{
         //force https
         if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -19,12 +35,23 @@ if (process.env.NODE_ENV === 'production') {
         next();
     });
 }
+// Add session to app
+const sessionMW = expressSession(session_config);
+app.use(sessionMW);
 
+// Configure passport
+require('./config/passport.js')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add routes
 app.use('/', require('./routes/home'));
 app.use('/account', require('./routes/account'));
 app.use('/admin', require('./routes/admin'));
