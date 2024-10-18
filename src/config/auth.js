29f0e8bd545b219ebfa098c26606a4781419d146
Darren Dignam <darren.dignam@gmail.com>
2024-10-18 09:11:59 +0000

    Improved the authenticaion module

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,14 +3,8 @@
 module.exports = {
     'googleAuth' : 
     {
-        clientID: "ID.apps.googleusercontent.com",
-        clientSecret: "secret",
-        callbackURL: "https://www.vipercloud.cc/account/google/return"
+        clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+        callbackURL: "https://www.vipercloud.cc/account/google/return/",
     },
-    'googleMailRelayAuth' : 
-    {
-        type: 'OAuth2',
-        clientId: process.env.GOOGLE_MAIL_CLIENTID,
-        clientSecret: process.env.GOOGLE_MAIL_CLIENT_SECRET,
-    }
 };
