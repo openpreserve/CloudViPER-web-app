65402044973ba9c7670801a588763780ab0a5434
Darren Dignam <darren.dignam@gmail.com>
2024-10-18 09:15:57 +0000

    Refactored the account route. Streamlined some functions, and added the routes needed for google oAuth

--- src/routes/account.js
+++ src/routes/account.js
@@ -7,63 +7,48 @@ const { Op } = require('sequelize');
 const authenticate = require('../utility/authenticate');
 const database = require('../utility/db.js');
 
-const gmail = require("../utility/email-relay.js")
+const mailer = require("../utility/email-relay.js");
 
+var passport = require('passport');
 var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
 
-// //prevent brute force attacks? Look at this:
-// var ExpressBrute = require('express-brute');
+const { sanitizeUsername } = require('../utility/helperFunctions.js');
 
-// function usernameToLowerCase(req, res, next) {
-//     req.body.username = req.body.username.toLowerCase();
-//     next();
-// }
-
-router.get('/', (req,res)=>{
+router.get('/', (req,res) => {
     if (req.user) {
+        const alertSuccess = req.flash('alert-success');
         res.render('user_account_edit', {
             // csrfToken: req.csrfToken(),
-            user: req.user
+            user: req.user,
+            alertSuccess: alertSuccess
         });
     } else {
         res.redirect('/account/login');
     }
 });
 
-//TODO: Update the html view.... but thats a given for the whole HTML version
-router.get('/register', function (req, res) {
-    // req.logout();//should prob logout?
+router.get('/register', (req, res) => {
     res.render('user_account_register');
 });
 
-router.post('/register', 
-// bruteforce.prevent, 
-function (req, res) {
+router.post('/register', (req, res) => {
     database.User.register(new database.User({
-        username: req.body.username,
-        nickname: req.body.username,
+        username: sanitizeUsername(req.body.username),
         role: "user",
         email: req.body.email,
         oauthProvider: "vipercloud",
         created: Date.now()
-    }), req.body.password, function (err, user) {
+    }), req.body.password, (err, user) => {
         if (err) {
-            console.log(err);
-            //return res.render('register', { account : account });
-            var responseData = {};
-            responseData["registered"] = 0;
-            responseData["message"] = "You are not registered";
-            responseData["error"] = err.code; //the error objects has a lot of info
-            responseData["data"] = {};
             var tmpErrMsg = "There was a problem creating your account. Please try again or contact our team who can assist.";
             res.format({
-                html: function () {
-                    console.log("sign up err: " + err.code);
-                    if (err.code == 11000) {
+                html: () => {
+                    console.log("sign up err: " + err.parent.errno);
+                    if (err.parent.errno == 1062) {
                         tmpErrMsg = "We are sorry but that email or username is already taken. If this is your account try resetting your password. Or use the social login.";
-                    } else if (err.message) {
-                        tmpErrMsg = err.message;
+                    } else if ( err.errors && err.errors[0].message ) {
+                        tmpErrMsg = err.errors[0].message;
                     }
                     res.render('user_account_register', {
                         error_message: tmpErrMsg,
@@ -72,37 +57,56 @@ function (req, res) {
                     });
                 },
                 //JSON response will show the newly created blob
-                json: function () {
+                json: () => {
                     res.json(responseData);
                 }
             });
         } else {
-            gmail.sendWelcomeEmail(req.body.email);
-
-            var responseData = {};
-            responseData["registered"] = 1;
-            responseData["message"] = "You are registered";
-            responseData["error"] = "";
-            responseData["data"] = {};
-
-            req.login(user, function (err) {
+            mailer.sendWelcomeEmail(req.body.email);
+            req.flash('alert-success', 'Thanks for setting up a ViPER account - you may need to contact an admin to get full access to the services on offer.');
+            req.login(user, (err) => {
                 if (!err) {
                     res.redirect('/account');
                 } else {
                     res.json(err);
                 }
             });
-
-
-
         }
     });
 });
 
-router.get('/login', function (req, res) {
+router.get('/login', (req, res) => {
     //Force log out? redirect if already logged in?
-    res.render('user_account_login');
+    const errorMsg = req.flash('error');
+    res.render('user_account_login', { error_message: errorMsg });
 });
 
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/account/login', failureFlash: true }),
+    (req, res) => {
+        if (req.user) {
+            res.redirect('/account');
+        } else {
+            console.log("Login failed... redirecting....");
+            res.redirect('/account/login');
+        }
+});
+
+router.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/account/login')
+    });
+});
+
+//Google oAuth routes
+router.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/return', passport.authenticate('google', { failureRedirect: '/account/login', failureFlash: true }),  (req, res) => {
+    req.flash('alert-success', 'Thanks for setting up a ViPER account - you may need to contact an admin to get full access to the services on offer.');
+    res.redirect('/account');
+});
 
 module.exports = router;
