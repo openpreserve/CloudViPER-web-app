a912c4aef384e0a10d0ff3defb73d6671b5ff265
Darren Dignam <darren.dignam@gmail.com>
2024-10-18 09:17:16 +0000

    Streamline the admin route, removing parts from the early test and dev phase of the project, now we have implemented a robust user account system with the passport module

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,17 +10,7 @@ const { getAvailablePort } = require('../utility/docker-port-generator.js');
 
 const authenticate = require('../utility/authenticate');
 const database = require('../utility/db.js');
-
-function generateRandomString(length) {
-  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
+const { generateRandomString } = require('../utility/helperFunctions.js');
 
 /* GET home page. */
 router.get('/', authenticate, (req, res)=>{
@@ -114,32 +104,5 @@ router.get('/terminate-instance/:containerId', async (req, res)=>{
 //     const data = await database.NPS.findAll();
 //     res.json(data);
 // });
-
-router.get('/login', (req,res)=>{
-    res.render('login', {
-        page_title: "Admin Login",
-        form_h1: "Please Login to access content....",
-        form_p: "", 
-      });
-});
-router.post('/login', (req,res)=>{
-    const admin_username = process.env.APP_USERNAME;
-    const admin_password = process.env.APP_PASSWORD;
-
-    if(admin_username==req.body.username && admin_password==req.body.password){
-        req.session.is_admin = true;
-        let redirectUrl = '/admin';
-        if(req.cookies && req.cookies.redirect_url != undefined){
-            redirectUrl = req.cookies.redirect_url;
-        }
-        
-        res.clearCookie('redirect_url'); // Clear the cookie
-        res.redirect(redirectUrl);
-    }else{
-        req.session.is_admin = false;
-        res.status(401).redirect('/admin/login');
-    }
-});
-
 
 module.exports = router;
