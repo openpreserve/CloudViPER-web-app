3cd515fd10f871eec7691b53863b3b51d032fac7
Darren Dignam <darren.dignam@gmail.com>
2024-12-11 21:23:40 +0000

    Using mySQL for session data now

--- src/config/auth.js
+++ src/config/auth.js
@@ -7,4 +7,13 @@ module.exports = {
         clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
         callbackURL: "https://www.vipercloud.cc/account/google/return/",
     },
+
+    'mysqlSessionAuth':
+    {   
+        host: process.env.DB_HOST, 
+        port: 3306, 
+        user: process.env.DB_USER, 
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
 };
