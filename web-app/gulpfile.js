7db63c502a2757a8b340daa7984c428c545014f6
Darren Dignam <darren.dignam@gmail.com>
2025-03-06 16:17:26 +0100

    Added gulp to make the build process better at handling the view rendering files. And could be useful later for more scripting tasks.

---
+++ web-app/gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const clean = require('gulp-clean');
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('clean-views', () => {
+  return gulp.src('dist/views', { read: false, allowEmpty: true })
+    .pipe(clean());
+});
+
+gulp.task('scripts', () => {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('views', () => {
+  return gulp.src('src/views/**/*')
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('default', gulp.series('scripts', 'clean-views', 'views'));
