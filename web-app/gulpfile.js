7269253dede7b8d4f6429e5455b5e12b5c276028
Darren Dignam <darren.dignam@gmail.com>
2025-03-06 20:30:52 +0100

    Added the public folder to the gulp build process

--- web-app/gulpfile.js
+++ web-app/gulpfile.js
@@ -2,11 +2,9 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const clean = require('gulp-clean');
 const tsProject = ts.createProject('tsconfig.json');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('clean-views', () => {
-  return gulp.src('dist/views', { read: false, allowEmpty: true })
-    .pipe(clean());
-});
+
 
 gulp.task('scripts', () => {
   return tsProject.src()
@@ -14,9 +12,24 @@ gulp.task('scripts', () => {
     .js.pipe(gulp.dest('dist'));
 });
 
+gulp.task('clean-views', () => {
+    return gulp.src('dist/views', { read: false, allowEmpty: true })
+      .pipe(clean());
+});
+
 gulp.task('views', () => {
   return gulp.src('src/views/**/*')
     .pipe(gulp.dest('dist/views'));
 });
 
-gulp.task('default', gulp.series('scripts', 'clean-views', 'views'));
+gulp.task('clean-public', () => {
+    return gulp.src('dist/public', { read: false, allowEmpty: true })
+      .pipe(clean());
+});
+
+gulp.task('public', () => {
+    return gulp.src('src/public/**/*', {encoding: false})
+        .pipe(gulp.dest('dist/public'));
+});
+
+gulp.task('default', gulp.series('scripts', 'clean-views', 'views', 'public'));
