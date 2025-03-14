9d0d9c59e961f224964e8d303fc48d837cb7acac
Darren Dignam <darren.dignam@gmail.com>
2025-03-14 14:27:33 +0100

    Updated package and build elements

---
+++ web-app/src/models/log.ts
@@ -0,0 +1,76 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+
+class Log extends Model {
+    public id!: number;
+    public eventType!: string;
+    public eventDescription!: string;
+    public message!: string;
+    public userId!: number | null;
+    public viperInstanceId!: number | null;
+    public browserInfo!: string | null;
+    public ipAddress!: string | null;
+    public dockerContainerId!: string | null;
+    public createdAt!: Date;
+    public updatedAt!: Date;
+}
+
+export default (sequelize: Sequelize) => {
+    Log.init(
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            eventType: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            eventDescription: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            message: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            userId: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                allowNull: true,
+            },
+            viperInstanceId: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                allowNull: true,
+            },
+            browserInfo: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            ipAddress: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            dockerContainerId: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+        },
+        {
+            sequelize,
+            tableName: 'Logs',
+            timestamps: true,
+        }
+    );
+
+    return Log;
+}
