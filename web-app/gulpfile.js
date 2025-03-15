a22f768c0a8e2b7d91a04a04f6092099848757a4
Darren Dignam <darren.dignam@gmail.com>
2025-03-15 13:59:36 +0100

    Added gulp to assist with compling and building the project

---
+++ web-app/gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const copy = require('gulp-copy');
+
+const tsProject = ts.createProject('tsconfig.json');
+
+// Clean the dist directory
+gulp.task('clean', () => {
+    return del(['dist']);
+});
+
+// Compile TypeScript files
+gulp.task('scripts', () => {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+});
+
+// Copy static files
+gulp.task('copy-views', () => {
+    return gulp.src('src/views/**/*')
+        .pipe(copy('dist/views', { prefix: 2 }));
+});
+
+gulp.task('copy-public', () => {
+    return gulp.src('src/public/**/*')
+        .pipe(copy('dist/public', { prefix: 2 }));
+});
+
+// Define the build task
+gulp.task('build', gulp.series('clean', 'scripts', 'copy-views', 'copy-public'));
+
+// Default task
+gulp.task('default', gulp.series('build'));
