feb85580d56f5a4defff81b292a10a51eb5f706b
Darren Dignam <darren.dignam@gmail.com>
2025-03-15 13:59:36 +0100

    Added gulp to assist with compling and building the project

--- web-app/gulpfile.js
+++ web-app/gulpfile.js
@@ -1,32 +1,39 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const clean = require('gulp-clean');
+const sourcemaps = require('gulp-sourcemaps');
+// const del = require('del');
+const copy = require('gulp-copy');
+
 const tsProject = ts.createProject('tsconfig.json');
 
+// Clean the dist directory
+// gulp.task('clean', () => {
+//     return del(['dist']);
+// });
+
+// Compile TypeScript files
 gulp.task('scripts', () => {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean-views', () => {
-    return gulp.src('dist/views', { read: false, allowEmpty: true })
-      .pipe(clean());
+// Copy static files
+gulp.task('copy-views', () => {
+    return gulp.src('src/views/**/*')
+        .pipe(copy('dist/views', { prefix: 2 }));
 });
 
-gulp.task('views', () => {
-  return gulp.src('src/views/**/*')
-    .pipe(gulp.dest('dist/views'));
+gulp.task('copy-public', () => {
+    return gulp.src('src/public/**/*')
+        .pipe(copy('dist/public', { prefix: 2 }));
 });
 
-gulp.task('clean-public', () => {
-    return gulp.src('dist/public', { read: false, allowEmpty: true })
-      .pipe(clean());
-});
+// Define the build task
+// gulp.task('build', gulp.series('clean', 'scripts', 'copy-views', 'copy-public'));
+gulp.task('build', gulp.series( 'scripts', 'copy-views', 'copy-public'));
 
-gulp.task('public', () => {
-    return gulp.src('src/public/**/*', {encoding: false})
-        .pipe(gulp.dest('dist/public'));
-});
-
-gulp.task('default', gulp.series('scripts', 'clean-views', 'views', 'public'));
+// Default task
+gulp.task('default', gulp.series('build'));
