d0ede5c08dd49f57143253fd68cfcb60e92188f0
Darren Dignam <darren.dignam@gmail.com>
2025-03-17 10:17:44 +0100

    Update the build script

--- web-app/gulpfile.js
+++ web-app/gulpfile.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
+// const del = require('del');
 const copy = require('gulp-copy');
 
 const tsProject = ts.createProject('tsconfig.json');
 
 // Clean the dist directory
-gulp.task('clean', () => {
-    return del(['dist']);
-});
+// gulp.task('clean', () => {
+//     return del(['dist']);
+// });
 
 // Compile TypeScript files
 gulp.task('scripts', () => {
@@ -32,7 +32,8 @@ gulp.task('copy-public', () => {
 });
 
 // Define the build task
-gulp.task('build', gulp.series('clean', 'scripts', 'copy-views', 'copy-public'));
+// gulp.task('build', gulp.series('clean', 'scripts', 'copy-views', 'copy-public'));
+gulp.task('build', gulp.series( 'scripts', 'copy-views', 'copy-public'));
 
 // Default task
 gulp.task('default', gulp.series('build'));
