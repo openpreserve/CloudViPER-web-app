63885bd8552536efbf537d34b48823d4b1beebed
Darren Dignam <darren.dignam@gmail.com>
2025-03-18 09:27:18 +0100

    Explicit definition of the database models, makes them easier to use.

--- web-app/src/models/index.ts
+++ web-app/src/models/index.ts
@@ -6,14 +6,13 @@ import * as process from 'process';
 import dotenv from 'dotenv';
 
 import configAuth from '../config/auth';
+import User from './user';
+import ViperInstance from './viperinstance';
 
 dotenv.config({ path: "../.env" });
-// console.log(process.env);
-
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const db: { [key: string]: any } = {};
 
 const databasehost = configAuth.mysqlSessionAuth.host;
 const database = configAuth.mysqlSessionAuth.database;
@@ -24,29 +23,32 @@ const config: { host: string; dialect: Dialect, logging: boolean | ((...msg: any
 let sequelize: Sequelize;
 sequelize = new Sequelize(database, username, password, config);
 
+const usermodel = User(sequelize);
+const vipermodel = ViperInstance(sequelize);
 
-const User = require('./user');
-const usermodel = User(sequelize, DataTypes);
-db[usermodel.name] = usermodel;
+interface DB {
+  sequelize: Sequelize;
+  Sequelize: typeof Sequelize;
+  User: typeof usermodel;
+  ViperInstance: typeof vipermodel;
+}
 
-const ViperInstance = require('./viperinstance');
-const vipermodel = ViperInstance(sequelize, DataTypes);
-db[vipermodel.name] = vipermodel;
+const db: DB = {
+  sequelize,
+  Sequelize,
+  User: usermodel,
+  ViperInstance: vipermodel,
+};
 
 Object.keys(db).forEach((modelName: string) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+  if ((db as any)[modelName].associate) {
+    (db as any)[modelName].associate(db);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
 db.sequelize.sync({ alter: true }).then(() => {
-
   console.log('Drop and Resync with { force: true }');
-  //console.error('Unable to create tables, shutting down...', error);
-  //process.exit(1);
 });
 
+export { usermodel, vipermodel };
 export default db;
