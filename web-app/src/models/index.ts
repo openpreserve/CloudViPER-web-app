5f7c65b429de83f2dc9b4a68a533ba4859415d9f
Darren Dignam <darren.dignam@gmail.com>
2025-03-19 11:23:38 +0100

    Added a logging table

--- web-app/src/models/index.ts
+++ web-app/src/models/index.ts
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 import configAuth from '../config/auth';
 import User from './user';
 import ViperInstance from './viperinstance';
+import Logs from './log'; // Import the Logs model
 
 dotenv.config({ path: "../.env" });
 
@@ -25,12 +26,14 @@ sequelize = new Sequelize(database, username, password, config);
 
 const usermodel = User(sequelize);
 const vipermodel = ViperInstance(sequelize);
+const logsmodel = Logs(sequelize); // Initialize the Logs model
 
 interface DB {
   sequelize: Sequelize;
   Sequelize: typeof Sequelize;
   User: typeof usermodel;
   ViperInstance: typeof vipermodel;
+  Logs: typeof logsmodel; // Add Logs to the DB interface
 }
 
 const db: DB = {
@@ -38,6 +41,7 @@ const db: DB = {
   Sequelize,
   User: usermodel,
   ViperInstance: vipermodel,
+  Logs: logsmodel, // Add Logs to the db object
 };
 
 Object.keys(db).forEach((modelName: string) => {
@@ -50,5 +54,5 @@ db.sequelize.sync({ alter: true }).then(() => {
   console.log('Database synchronized with { alter: true }');
 });
 
-export { usermodel, vipermodel };
+export { usermodel, vipermodel, logsmodel }; // Export logsmodel
 export default db;
