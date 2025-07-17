fd4b97885c49593cca0a5271e374ac6a2c5d61cd
Darren Dignam <darren.dignam@gmail.com>
2025-07-17 16:12:13 +0200

    Checking in the test components

--- web-app/jest.config.js
+++ web-app/jest.config.js
@@ -2,6 +2,21 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/test/**/*.test.ts'], // Matches files in the test folder ending in .test.ts
-    collectCoverage: true, // Enable code coverage
-    coverageDirectory: 'coverage', // Output directory for coverage reports
+    // collectCoverage: true, // Enable code coverage
+    // coverageDirectory: 'coverage', // Output directory for coverage reports
+
+    // collectCoverage: true, // Enable code coverage
+    // coverageDirectory: 'coverage', // Output directory for coverage reports
+    // collectCoverageFrom: ['src/**/*.ts'], // Files to collect coverage from
+    // coveragePathIgnorePatterns: ['/node_modules/', '/test/'], // Files to ignore in coverage reports
+    // moduleFileExtensions: ['ts', 'js'], // File extensions to consider
+    // moduleNameMapper: {
+    //     '^@/(.*)$': '<rootDir>/src/$1',
+    // },
+    // transform: {
+    //     '^.+\\.ts$': 'ts-jest',
+    // },
+    verbose: true,
+    forceExit: true,
+    // clearMocks: true,
 };
